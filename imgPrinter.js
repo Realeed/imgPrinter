31b81a8c407dad2136ae25d5199728e78d404e98
Realeed <hsargsyan395@gmail.com>
2021-02-09 14:00:56 +0400

    last

--- imgPrinter.js
+++ imgPrinter.js
@@ -17,14 +17,28 @@ addEventListener('DOMContentLoaded', () => {
             width.style.outline = 'none';
             width.style.position = 'fixed'
             width.style.fontSize = '40px';
-            width.style.width = '275px';
-            width.style.marginLeft = '60%'
+            width.style.width = '285px';
+            width.style.marginLeft = '75%'
             document.body.appendChild(width);
+            let height = document.createElement('input');
+            height.placeholder = 'Height in pixels';
+            height.type = 'number';
+            height.min = '0';
+            height.max = '1000'
+            height.step = '10';
+            height.autofocus = true;
+            height.style.marginTop = '140px';
+            height.style.outline = 'none';
+            height.style.position = 'fixed'
+            height.style.fontSize = '40px';
+            height.style.width = '285px';
+            height.style.marginLeft = '75%'
+            document.body.appendChild(height)
             let next = document.createElement('button');
             next.id = 'but'
             next.innerHTML = 'Next'
-            next.style.marginLeft = '60%';
-            next.style.marginTop = '140px';
+            next.style.marginLeft = '75%';
+            next.style.marginTop = '230px';
             next.style.fontSize = '35px';
             next.style.position = 'fixed'
             next.style.paddingLeft = '25px';
@@ -35,7 +49,7 @@ addEventListener('DOMContentLoaded', () => {
             document.body.appendChild(pass)
             let img = document.createElement('img');
             img.src = URL.createObjectURL(event.target.files[0]);
-            img.style.width = '200px';
+            img.style.width = '500px';
             img.style.marginTop = '30px';
             document.body.appendChild(img);
             document.getElementsByTagName('input')[1].disabled = true;
@@ -43,46 +57,79 @@ addEventListener('DOMContentLoaded', () => {
             width.onchange = () => {
                 let wpix = width.value + 'px'
                 img.style.width = wpix;
-                if (width.value == '') {
-                    img.style.width = '200px'
+                if (width.value == '' && height.value == '') {
+                    img.style.width = '500px';
+                    img.style.height = 'auto';
                     next.disabled = true;
                     next.style.cursor = 'default';
-                } else if (width.value > 1000) {
-                    alert('Entered value must be less than or equal to 1000!');
-                    img.style.width = '200px';
+                } else if (width.value == 0) {
                     width.value = '';
-                    next.disabled = true;
-                    next.style.cursor = 'default';
                 } else {
                     next.disabled = false;
                     next.style.cursor = 'pointer';
                 }
-                if (width.value == 0) {
-                    next.disabled = true;
-                    next.style.cursor = 'default';
+                if (width.value == '' && height.value == '') {
+                    img.style.width = '500px';
+                    img.style.height = 'auto';
                 }
             }
             width.onkeyup = () => {
                 let wpix = width.value + 'px'
                 img.style.width = wpix;
-                if (width.value == '') {
-                    img.style.width = '200px'
+                if (width.value == '' && height.value == '') {
+                    img.style.width = '500px';
+                    img.style.height = 'auto';
                     next.disabled = true;
                     next.style.cursor = 'default';
                 } else if (width.value > 1000) {
                     alert('Entered value must be less than or equal to 1000!');
-                    img.style.width = '200px';
                     width.value = '';
+                    height.value = '';
+                    img.style.width = '500px';
+                    img.style.height = 'auto';
+                    next.disabled = true;
+                    next.style.cursor = 'default';
+                } else if (width.value == 0) {
+                    width.value = '';
+                } else {
+                    next.disabled = false;
+                    next.style.cursor = 'pointer';
+                }
+                if (width.value == '' && height.value != '') {
+                    next.disabled = false;
+                    next.style.cursor = 'pointer';
+                }
+                if (width.value == '' && height.value == '') {
+                    img.style.width = '500px';
+                    img.style.height = 'auto';
+                }
+            }
+            height.onchange = () => {
+                let hpix = height.value + 'px'
+                img.style.height = hpix;
+                if (height.value == '' && width.value == '') {
+                    next.disabled = true;
+                    next.style.cursor = 'default';
+                } else if (height.value == 0 && width.value == '') {
                     next.disabled = true;
                     next.style.cursor = 'default';
                 } else {
                     next.disabled = false;
                     next.style.cursor = 'pointer';
                 }
-                if (width.value == 0) {
+            }
+            height.onkeyup = () => {
+                let hpix = height.value + 'px'
+                img.style.height = hpix;
+                if (height.value == '' && width.value == '') {
                     next.disabled = true;
                     next.style.cursor = 'default';
-                    width.value = '';
+                } else if (height.value == 0) {
+                    next.style.cursor = 'default';
+                    height.value = '';
+                } else {
+                    next.disabled = false;
+                    next.style.cursor = 'pointer';
                 }
             }
             next.onclick = () => {
@@ -90,6 +137,7 @@ addEventListener('DOMContentLoaded', () => {
                 next.disabled = true;
                 next.style.display = 'none';
                 width.style.display = 'none';
+                height.style.display = 'none';
                 img.src = '';
                 document.body.appendChild(img)
                 document.getElementsByTagName('body')[0].style.width = '100%';
@@ -134,6 +182,7 @@ addEventListener('DOMContentLoaded', () => {
                     document.getElementsByTagName('form')[0].innerHTML = '';
                     next.style.display = 'none';
                     width.style.display = 'none';
+                    height.style.display = 'none';
                     img.src = ''
                     document.body.appendChild(img)
                     document.getElementsByTagName('body')[0].style.width = '100%';
